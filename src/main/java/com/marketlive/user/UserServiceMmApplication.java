88daa0b77303b67cec11ca827b0452807ee6a67b
Rakesh <49619785+rroushan@users.noreply.github.com>
2025-02-02 18:14:50 +0530

    Add files via upload

---
+++ src/main/java/com/marketlive/user/UserServiceMmApplication.java
@@ -0,0 +1,13 @@
+package com.marketlive.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceMmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketlive/user/UserServiceMmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketlive.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceMmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
